92ffa5b94e333ffbca35d2d873fd25922f3daaab
tolgaparlan <tolgaparlan@gmail.com>
2018-03-09 15:30:20 +0100

    should save authtoken to the model

--- authdemo/authcode/models.py
+++ authdemo/authcode/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 
 # Create your models here.
-class User(models.Model):
+class InstagramClient(models.Model):
   client_id = models.CharField(max_length=35)
   redirect_uri = models.CharField(max_length=300)
+
+class AuthenticationToken(models.Model):
+  token = models.CharField(max_length=100)
+  date = models.DateTimeField('date acquired')
--- authdemo/authcode/views.py
+++ authdemo/authcode/views.py
@@ -1,27 +1,30 @@
 from django.shortcuts import render
-from .models import User
-# from django.http import HttpResponseRedirect
-# from django.urls import reverse
+from .models import InstagramClient, AuthenticationToken
+from django.utils import timezone
 
 
 def get_auth_code(request):
   if 'code' in request.GET:
-    ret = {'code': request.GET['code']}
-  else:
-    ret = {'error_message': 'No Code'}
+    newToken = AuthenticationToken(
+        token=request.GET['code'], date=timezone.now())
+    newToken.save()
+  #   ret = {'code': request.GET['code']}
+  # else:
+  #   ret = {'error_message': 'No Code'}
 
-  return render(request, 'authcode/received.html', ret)
+  return render(request, 'admin')
+
 
 def user_list(request):
-  users = User.objects.values()
-  context = {'user_list':users}
+  clients = InstagramClient.objects.values()
+  context = {'user_list': clients}
   return render(request, 'authcode/user_list.html', context)
 
 # def send_auth_request(request):
 #   context = {'response_type':'code'}
 #   if 'client_id' in request.GET and 'redirect_uri' in request.GET:
 #     context['client_id'] = request.GET['client_id']
-#     context['redirect_uri'] = request.GET['redirect_uri']    
+#     context['redirect_uri'] = request.GET['redirect_uri']
 
 #   # return render(request, 'https://api.instagram.com/oauth/authorize', context)
 #   # return HttpResponseRedirect('https://api.instagram.com/oauth/authorize', args=context)
