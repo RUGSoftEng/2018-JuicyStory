9fc0ce69e7a749afb21da3999441e57a4df7c23f
George Argyrousis <g.argyrousis@student.rug.nl>
2018-03-09 22:16:02 +0100

    Adding security features

--- authdemo/authdemo/views.py
+++ authdemo/authdemo/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
+#this will redirect us to the login page
+@login_required(login_url="/users/login/")
 def home(request):
 	return render(request, "home.html")
