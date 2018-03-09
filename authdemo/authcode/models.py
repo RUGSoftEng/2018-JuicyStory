c09c36d9abda922cfc1b8e2fd3e059669c064b16
tolgaparlan <tolgaparlan@gmail.com>
2018-03-09 12:23:07 +0100

    user_list page can now send requests for auth_code

--- authdemo/authcode/models.py
+++ authdemo/authcode/models.py
@@ -2,6 +2,5 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-  client_id = models.CharField(max_length=30)
-  redirect_uri = models.CharField(max_length=100)
-  
+  client_id = models.CharField(max_length=35)
+  redirect_uri = models.CharField(max_length=300)
--- authdemo/authcode/views.py
+++ authdemo/authcode/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from .models import User
+# from django.http import HttpResponseRedirect
+# from django.urls import reverse
+
 
 def get_auth_code(request):
   if 'code' in request.GET:
@@ -13,3 +16,13 @@ def user_list(request):
   users = User.objects.values()
   context = {'user_list':users}
   return render(request, 'authcode/user_list.html', context)
+
+# def send_auth_request(request):
+#   context = {'response_type':'code'}
+#   if 'client_id' in request.GET and 'redirect_uri' in request.GET:
+#     context['client_id'] = request.GET['client_id']
+#     context['redirect_uri'] = request.GET['redirect_uri']    
+
+#   # return render(request, 'https://api.instagram.com/oauth/authorize', context)
+#   # return HttpResponseRedirect('https://api.instagram.com/oauth/authorize', args=context)
+#   return HttpResponseRedirect(reverse('https://api.instagram.com/oauth/authorize', kwargs=context))
