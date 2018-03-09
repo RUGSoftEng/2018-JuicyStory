7969a818803e9c2c6575d3e8497c0fdbf9ca083f
tolgaparlan <tolgaparlan@gmail.com>
2018-03-09 18:11:05 +0100

    implemented access_token retrieval

--- authdemo/authcode/models.py
+++ authdemo/authcode/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # We currently have only one client
 class InstagramClient(models.Model):
   client_id = models.CharField(max_length=50)
-  client_secret = models.CharField(max_length=50)
+  client_secret = models.CharField(max_length=50, default="")
   redirect_uri = models.CharField(max_length=300)
 
 # Boaty Macboatface only currently
--- authdemo/authcode/views.py
+++ authdemo/authcode/views.py
@@ -6,10 +6,10 @@ from .models import InstagramClient, InstagramUser
 def get_access_token(auth_code):
   url = "https://api.instagram.com/oauth/access_token"
   client = InstagramClient.objects.get(pk=1)
-  post_data = {'client_id': client.id, 'client_secret': client.secret,
+  post_data = {'client_id': client.client_id, 'client_secret': client.client_secret,
                'grant_type': 'authorization_code', 'redirect_uri': client.redirect_uri, 'code': auth_code}
-  response = requests.post(url, post_data)
-  content = response.content
+  response = requests.post(url, data=post_data)
+  content = response.json()
   return content
 
 
@@ -17,17 +17,21 @@ def process_auth_code(request):
   if 'code' in request.GET:
     code = request.GET['code']
     response = get_access_token(code)
-    access_token = response['access_token']
-    username = response['user']['id']
+    if "error_type" in response:
+      print(response)
+    else: 
+      access_token = response['access_token']
+      username = response['user']['username']
+      print(username)
 
-    user = InstagramUser.objects.get(username=username)
-    user.access_token = access_token
-    user.save()
+      user = InstagramUser.objects.get(username=username)
+      user.access_token = access_token
+      user.save()
 
   return user_list(request)
 
 
 def user_list(request):
   clients = InstagramClient.objects.values()
-  context = {'user_list': clients}
+  context = {'user': clients[0]}
   return render(request, 'authcode/user_list.html', context)
