450230047e554b6dcfe3d807127ecac0a9e2cb98
tolgaparlan <tolgaparlan@gmail.com>
2018-03-11 12:49:24 +0100

    designed most of the image acquisition via location

--- authdemo/location/utils.py
+++ authdemo/location/utils.py
@@ -1,9 +1,11 @@
 import requests
 
+
 def get_self_user_info(access_token):
   url = "https://api.instagram.com/v1/users/self/"
-  response = requests.get(url, params={"access_token":access_token})
+  response = requests.get(url, params={"access_token": access_token})
   return response.json()
+
 
 def request_images_by_tag(tag, access_token):
   url = "https://api.instagram.com/v1/tags/%s/media/recent" % tag
@@ -12,23 +14,13 @@ def request_images_by_tag(tag, access_token):
   return response.json()
 
 
-def get_geography_by_address(address):
-  url = "https://maps.google.com/maps/api/geocode/json"
-  response = requests.get(url, params={'address': address})
-  return response.json()['results'][0]['geometry']['location']
-
-
-def request_images_by_location(location, access_token):
+def request_ids_by_coordinate(coordinates, access_token):
   search_url = 'https://api.instagram.com/v1/locations/search'
-  # try:
-  geography = get_geography_by_address(location)
   response = requests.get(search_url, params={
-                          'lat': geography['lat'], 'lng': geography['lng'], 'access_token': access_token, 'distance': 500})
-  location_id = response.json()['data'][0]['id']
-  return (response.json())
-  location_url = 'https://api.instagram.com/v1/locations/%s/media/recent' % location_id
-  response = requests.get(location_url, params={'access_token': access_token})
-  return response.json()
+                          'lat': coordinates['lat'], 'lng': coordinates['lng'], 'access_token': access_token, 'distance': 500})
+  return response.json()['data'][:5]
 
-  # except:
-  #   return {'error': 'Error while getting the images'}
+def request_images_by_location_id(location_id, access_token):
+  url = 'https://api.instagram.com/v1/locations/%s/media/recent' % location_id
+  response = requests.get(url, params={'access_token': access_token})
+  return response.json()['data']
--- authdemo/location/views.py
+++ authdemo/location/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from authcode.models import InstagramUser
-from django.http import JsonResponse
-from .utils import request_images_by_tag, request_images_by_location, get_self_user_info
+# from django.http import JsonResponse
+from .utils import *
 
 
 def list_images(request, username):
@@ -9,16 +9,17 @@ def list_images(request, username):
   access_token = user.access_token
   userdata = get_self_user_info(access_token)["data"]
   images = {}
+  places = {}
 
   if 'tag_or_location' in request.GET:
     if request.GET['tag_or_location'][0] == '#':
       tag = request.GET['tag_or_location'][1:]
       images = request_images_by_tag(tag, access_token)['data']
     else:
-      pass
-      # location = request.GET['tag_or_location']
-      # images = request_images_by_location(location, access_token)
+      location = {"lat": "53.2383152", "lng": "6.5349696"} # TODO: Don't hardcode
+      places = request_ids_by_coordinate(location, access_token)
 
-    # return JsonResponse(images)
+  elif 'id' in request.GET:
+    images = request_images_by_location_id(request.GET['id'], access_token)
 
-  return render(request, 'location/list_images.html', {'userdata': userdata, 'images': images})
+  return render(request, 'location/list_images.html', {'userdata': userdata, 'images': images, 'places':places})
