fd4bb88162ea15c674a55a6705abeac198626ca1
tolgaparlan <tolgaparlan@gmail.com>
2018-03-18 20:11:30 +0100

    created a cron job

--- authdemo/upload/cron.py
+++ authdemo/upload/cron.py
@@ -1,11 +1,13 @@
 from django_cron import CronJobBase, Schedule
 from .models import Image
+import datetime
+
 
 class MyCronJob(CronJobBase):
   RUN_EVERY_MINS = 1
 
   schedule = Schedule(run_every_mins=RUN_EVERY_MINS)
-  code = 'upload.my_cron_job'  
+  code = 'upload.my_cron_job'
 
   def do(self):
-    Image.objects.all().delete()
+    Image.objects.filter(upload_date__lte=datetime.datetime.now()).delete()
--- authdemo/upload/views.py
+++ authdemo/upload/views.py
@@ -4,6 +4,7 @@ from authcode.models import InstagramUser
 from .utils import *
 from .forms import ImageUploadForm
 from .models import Image
+import datetime
 
 def upload_page(request, username):
   user = get_object_or_404(InstagramUser, username=username)
@@ -12,7 +13,7 @@ def upload_page(request, username):
   if(request.method == 'POST'):
     form = ImageUploadForm(request.POST, request.FILES)
     if form.is_valid:
-      newImage = Image(username = user.username, image_file = request.FILES['file'])
+      newImage = Image(username = user.username, image_file = request.FILES['file'], upload_date = datetime.datetime.now())
       newImage.save()
 
       return HttpResponseRedirect(reverse('upload:upload_page', kwargs = context))
