aea7c7d6ffec077d83101953be62d2621e1cf9f7
George Argyrousis <g.argyrousis@student.rug.nl>
2018-03-30 19:29:39 +0200

    merged account list

--- authdemo/iusers/urls.py
+++ authdemo/iusers/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = 'iusers'
 
 urlpatterns = [
-	path('addIuser/', views.add_iuser, name='add_iuser'),
+	#path('addIuser/', views.add_iuser, name='add_iuser'),
 	path('listIusers/', views.list_iusers, name='list_iusers'),
 ]
--- authdemo/iusers/views.py
+++ authdemo/iusers/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth.decorators import login_required
 from . import forms
 from authcode.models import InstagramUser
 
+
+'''
 # if the user calls the function from the urls, he will go to the else
 # statement and show the html with the form. If he is already in the html
 # and he submits then we redirect him to the page listing all instagram accs.
@@ -18,9 +20,19 @@ def add_iuser(request):
 	else:
 		form = forms.AddIuser()
 	return render(request, 'iusers/add-iuser.html', {'form':form})
+'''
 
 @login_required(login_url='/users/login/')
 def list_iusers(request):
+	if request.method == 'POST':
+		form = forms.AddIuser(request.POST, request.FILES)
+		if form.is_valid():
+			instance = form.save(commit=False)
+			instance.owner = request.user
+			instance.save()
+			return redirect('iusers:list_iusers')
+	else:
+		form = forms.AddIuser()
 	iusers = InstagramUser.objects.filter(owner=request.user)
-	return render(request, 'iusers/iuser-list.html', {'iusers':iusers})
+	return render(request, 'iusers/iuser-list.html', {'iusers':iusers, 'form':form})
 
