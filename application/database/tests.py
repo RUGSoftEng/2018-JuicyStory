f054483f5e31d85ad8c7bb31387a80dd75f0d458
George Argyrousis <g.argyrousis@student.rug.nl>
2018-05-04 10:29:05 +0200

    Testing database

--- application/database/tests.py
+++ application/database/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from .models import InstagramUser
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class DatabaseTests(TestCase):
+
+	#Set up 1000 random users and test them.
+	def setUp(self):
+		size = 1000
+		george = User.objects.create_user('testname', 'test@mail.com', 'testpassword')
+		for i in range(0, size):
+			username = "Test" + str(i)
+			#print(username)
+			InstagramUser.objects.create(username=username, owner=george)
+
+	#The actual test of asserting that the names match
+	def test_users(self):
+		size = 1000
+		for i in range(0, size):
+			username = "Test" + str(i)
+			iuser = InstagramUser.objects.get(username=username)
+			self.assertEqual(iuser.username, username)
