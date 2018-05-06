b8c093f673b8e7b558451d7fd773c2a72204be88
georak1 <grakshiev@gmail.com>
2018-05-06 15:03:21 +0200

    Update views.py
    removed test print statement

--- application/statistics/views.py
+++ application/statistics/views.py
@@ -17,7 +17,7 @@ def get_profile_stats(request,iusername,timeFrame,timeStampSince,timeStampUntil)
 def get_views_and_count(request,iusername,timeStampSince,timeStampUntil):
 	user = get_object_or_404(InstagramUser, username=iusername)
 	userData = request_views_and_followers(user.fbtoken,user.fbid,timeStampSince,timeStampUntil)
-	print(userData)
+	
 	context = {'userdata': userData, 'instagram_user':user}
 	return render(request, "statistics/statistics.html",context)
 
