1bcc24a70262118b40c9637da4b863a7814feebc
George Argyrousis <g.argyrousis@student.rug.nl>
2018-05-14 19:24:04 +0200

    modified the list_images function to display dm's based on input

--- application/incoming/views.py
+++ application/incoming/views.py
@@ -10,41 +10,6 @@ def list_images(request, iusername):
 
   # got an error, try to acquire a new access token
   if "data" not in userdata:
-    return redirect('authcode:get_code')    
-
-  userdata = userdata["data"]
-  # images from the request
-  images = {}
-  places = {}
-
-  if 'tag_or_location' in request.GET:
-    if request.GET['tag_or_location'][0] == '#':
-      tag = request.GET['tag_or_location'][1:]
-      images = request_images_by_tag(tag, access_token)
-    else:
-      location = request.GET['tag_or_location']
-      places = query_locations_by_name(location)
-  elif 'id' in request.GET:
-    images = request_images_by_location_id(request.GET['id'], access_token)
-
-  print(images)
-
-  context = {
-  'userdata': userdata,
-  'images': images,
-  'places':places,
-  'instagram_user':user}
-
-  return render(request, 'incoming/incoming_images.html', context)
-
-def list_dm_images(request, iusername):
-  user = get_object_or_404(InstagramUser, username=iusername)
-  access_token = user.access_token
-  userdata = get_self_user_info(access_token)
-  print(userdata)
-
-  # got an error, try to acquire a new access token
-  if "data" not in userdata:
     return redirect('authentication:get_code')
 
   # Get the DMs
@@ -54,25 +19,24 @@ def list_dm_images(request, iusername):
 
   DMResponse = instagram_api.last_json
   userdata = userdata["data"]
-  print(userdata)
   images = {}
   places = {}
   DMImages = {}
-
-  for messageThread in DMResponse['inbox']['threads']:
-    for item in messageThread['items']:
-      if 'media' in item:
-        DMImages[item['timestamp']] = item['media']['image_versions2']['candidates'][0]['url']
-  orderedDMTimestamps = sorted(DMImages.keys())
+  orderedDMTimestamps = {}
 
   if 'tag_or_location' in request.GET:
     if request.GET['tag_or_location'][0] == '#':
       tag = request.GET['tag_or_location'][1:]
       images = request_images_by_tag(tag, access_token)
+    elif request.GET['tag_or_location'][0] == '$':
+      for messageThread in DMResponse['inbox']['threads']:
+        for item in messageThread['items']:
+          if 'media' in item:
+            DMImages[item['timestamp']] = item['media']['image_versions2']['candidates'][0]['url']
+      orderedDMTimestamps = sorted(DMImages.keys())
     else:
       location = request.GET['tag_or_location']
       places = query_locations_by_name(location)
-
   elif 'id' in request.GET:
     images = request_images_by_location_id(request.GET['id'], access_token)
 
