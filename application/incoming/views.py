5559fb431483b1c290c3cac6a63ed4feee392303
tolgaparlan <tolgaparlan@gmail.com>
2018-05-14 10:42:16 +0200

    formatting fixes

--- application/incoming/views.py
+++ application/incoming/views.py
@@ -5,53 +5,58 @@ from .utils import get_self_user_info, request_images_by_tag, request_images_by_
 
 
 def list_images(request, iusername):
-    user = get_object_or_404(InstagramUser, username=iusername)
-    access_token = user.access_token
-    userdata = get_self_user_info(access_token)
+  user = get_object_or_404(InstagramUser, username=iusername)
+  access_token = user.access_token
+  userdata = get_self_user_info(access_token)
 
-    # got an error, try to acquire a new access token
-    if "data" not in userdata:
-        return redirect('authentication:get_code')
+  # got an error, try to acquire a new access token
+  if "data" not in userdata:
+    return redirect('authentication:get_code')
 
-    # Get the DMs
-    instagram_api = InstagramAPI(user)
-    instagram_api.login()
-    instagram_api.get_v2_inbox()
+  # Get the DMs
+  instagram_api = InstagramAPI(user)
+  instagram_api.login()
+  instagram_api.get_v2_inbox()
 
-    DMResponse = instagram_api.last_json
-    userdata = userdata["data"]
-    images = {}
-    places = {}
-    DMImages = {}
+  DMResponse = instagram_api.last_json
+  userdata = userdata["data"]
+  images = {}
+  places = {}
+  DMImages = {}
 
-    for messageThread in DMResponse['inbox']['threads']:
-        for item in messageThread['items']:
-            if 'media' in item:
-                DMImages[item['timestamp']
-                         ] = item['media']['image_versions2']['candidates'][0]['url']
-    orderedDMTimestamps = sorted(DMImages.keys())
+  for messageThread in DMResponse['inbox']['threads']:
+    for item in messageThread['items']:
+      if 'media' in item:
+        DMImages[item['timestamp']] = item['media']['image_versions2']['candidates'][0]['url']
+  orderedDMTimestamps = sorted(DMImages.keys())
 
-    if 'tag_or_location' in request.GET:
-        if request.GET['tag_or_location'][0] == '#':
-            tag = request.GET['tag_or_location'][1:]
-            images = request_images_by_tag(tag, access_token)
-        else:
-            location = request.GET['tag_or_location']
-            places = query_locations_by_name(location)
+  if 'tag_or_location' in request.GET:
+    if request.GET['tag_or_location'][0] == '#':
+      tag = request.GET['tag_or_location'][1:]
+      images = request_images_by_tag(tag, access_token)
+    else:
+      location = request.GET['tag_or_location']
+      places = query_locations_by_name(location)
 
-    elif 'id' in request.GET:
-        images = request_images_by_location_id(request.GET['id'], access_token)
+  elif 'id' in request.GET:
+    images = request_images_by_location_id(request.GET['id'], access_token)
 
-    context = {'userdata': userdata, 'images': images,
-               'places': places, 'DMImages': DMImages, 'orderedDMTimestamps': orderedDMTimestamps, 'instagram_user': user}
-    return render(request, 'incoming/incoming_images.html', context)
+  context = {
+    'userdata': userdata,
+    'images': images,
+    'places': places,
+    'DMImages': DMImages,
+    'orderedDMTimestamps': orderedDMTimestamps,
+    'instagram_user': user
+  }
+  return render(request, 'incoming/incoming_images.html', context)
 
 
 # Select an image and save it in the databse
 def select_images(request, iusername):
-    user = get_object_or_404(InstagramUser, username=iusername)
-    image_url = request.POST.getlist('url')
-    for url in image_url:
-        if url:
-            SelectedImage.objects.create(instagram_user=user, photo=url)
-    return redirect('upload:list_selected_images', iusername)
+  user = get_object_or_404(InstagramUser, username=iusername)
+  image_url = request.POST.getlist('url')
+  for url in image_url:
+    if url:
+      SelectedImage.objects.create(instagram_user=user, photo=url)
+  return redirect('upload:list_selected_images', iusername)
--- application/incoming/viewsets.py
+++ application/incoming/viewsets.py
@@ -5,60 +5,58 @@ from .utils import list_images
 
 
 class IncomingViewSet(viewsets.ViewSet):
-    """ Viewset to handle requests made regarding the incoming images of instagram users. """
+  """ Viewset to handle requests made regarding the incoming images of instagram users. """
 
-    def list(self, request):
-        """ GET the list of incoming images for the given instagram user. """
+  def list(self, request):
+    """ GET the list of incoming images for the given instagram user. """
 
-        if "instagram_user_name" in request.GET:
-            instagram_user_name = request.GET["instagram_user_name"]
-        else:
-            raise ValidationError(
-                detail="instagram_user_name parameter is mandatory.")
+    if "instagram_user_name" in request.GET:
+      instagram_user_name = request.GET["instagram_user_name"]
+    else:
+      raise ValidationError(detail="instagram_user_name parameter is mandatory.")
 
-        tag = None
-        location_id = None
-        get_DM = None
+    tag = None
+    location_id = None
+    get_DM = None
 
-        if "tag" in request.GET:
-            tag = request.GET["tag"]
+    if "tag" in request.GET:
+      tag = request.GET["tag"]
 
-        if "location_id" in request.GET:
-            location_id = request.GET["location_id"]
+    if "location_id" in request.GET:
+      location_id = request.GET["location_id"]
 
-        if "get_DM" in request.GET:
-            get_DM = request.GET["get_DM"]
+    if "get_DM" in request.GET:
+      get_DM = request.GET["get_DM"]
 
-        data = list_images(instagram_user_name, tag=tag,
-                           location_id=location_id, get_DM=get_DM)
+    data = list_images(instagram_user_name, tag=tag, location_id=location_id, get_DM=get_DM)
 
-        if "error" in data:
-            return Response(data={"Error": data["error"]}, exception=True, status=400)
+    if "error" in data:
+      return Response(data={"Error": data["error"]}, exception=True, status=400)
 
-        return Response(data)
+    return Response(data)
 
 
 class LocationQueryViewSet(viewsets.ViewSet):
-    """ Viewset to handle requests made to get location details """
+  """ Viewset to handle requests made to get location details """
 
-    def list(self, request):
-        """ GET the list of location names and their IDs for a passed location string. """
+  def list(self, request):
+    """ GET the list of location names and their IDs for a passed location string. """
 
-        if "location" in request.GET:
-            location = request.GET["location"]
-        else:
-            raise ValidationError(detail="location parameter is mandatory.")
+    if "location" in request.GET:
+      location = request.GET["location"]
+    else:
+      raise ValidationError(detail="location parameter is mandatory.")
 
-        count = 10  # the default value
+    count = 10  # the default value
 
-        if "count" in request.GET:
-            count = request.GET["count"]
+    if "count" in request.GET:
+      count = request.GET["count"]
 
-        # data = query_locations_by_name(location)
-        # COMMENTED OUT BECAUSE WE NEED EXTRA APP PERMISSIONS FOR THIS RIGHT NOW
-        data = {"error": "Waiting for extra app permissions right now."}
+    # data = query_locations_by_name(location)
+    # COMMENTED OUT BECAUSE WE NEED EXTRA APP PERMISSIONS FOR THIS RIGHT NOW
+    data = {"error": "Waiting for extra app permissions right now."}
 
-        if "error" in data:
-            return Response(data={"Error": data["error"]}, exception=True, status=400)
+    if "error" in data:
+      return Response(data={"Error": data["error"]}, exception=True, status=400)
 
-        return Response(data["data"][:count])
+    return Response(data["data"][:count])
