f8711d4aedee12edd7577e8215b2b7c2a0fb08c8
tolgaparlan <tolgaparlan@gmail.com>
2018-06-02 11:47:59 +0200

    simple fixes for upload module

--- application/upload/urls.py
+++ application/upload/urls.py
@@ -4,6 +4,4 @@ from . import views
 app_name = "upload"
 
 urlpatterns = [
-  path('list_selected_images/', views.list_selected_images, name='list_selected_images'),
-  path('process_selected_images/', views.process_selected_images, name='process_selected_images')
 ]
