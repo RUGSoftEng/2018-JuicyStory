de67bd0bd89a756643110bd7b78bc0ff228e8bb8
tolgaparlan <tolgaparlan@gmail.com>
2018-06-05 10:00:43 +0200

    got the story upload working with new api

--- application/upload/views.py
+++ application/upload/views.py
@@ -3,7 +3,8 @@ from rest_framework.status import (HTTP_200_OK, HTTP_400_BAD_REQUEST)
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from .serializers import PostImageToStorySerializer
-from .utils import upload_image
+from .utils import upload_image, download_and_schedule_image
+
 
 class PostImageToStory(APIView):
   ''' def post(self, request, iuser, photo_url): '''
@@ -11,16 +12,16 @@ class PostImageToStory(APIView):
   permission_classes = (IsAuthenticated,)
 
   def post(self, request, *args, **kwargs):
-  	data = request.data
-  	serializer = PostImageToStorySerializer(data=data)
+    data = request.data
+    serializer = PostImageToStorySerializer(data=data)
 
-  	if serializer.is_valid(raise_exception=True):
-  		valid_data = serializer.data
-  		iusername = valid_data["iusername"]
-  		photo_url = valid_data["photo_url"]
-  		upload_image(iusername, photo_url)
-  		return Response(valid_data, status=HTTP_200_OK)
-  	else:
-  		return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
-
-
+    if serializer.is_valid(raise_exception=True):
+      valid_data = serializer.data
+      iusername = valid_data["iusername"]
+      photo_url = valid_data["photo_url"]
+      # photo = download_image(photo_url)
+      # upload_image(iusername, photo)
+      download_and_schedule_image(photo_url, iusername, "2000-10-10", "00:00", is_story=True)
+      return Response(valid_data, status=HTTP_200_OK)
+    else:
+      return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
